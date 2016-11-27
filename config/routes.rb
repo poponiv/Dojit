69c681e3ae504816570a27d51921167171543426
nivd <niv@simplee.com>
2016-11-27 17:47:00 +0200

    Finished CRUD checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  # get 'posts/index'
 
-  get 'welcome/about'
+  # get 'posts/show'
 
-  get 'welcome/contact'
+  # get 'posts/new'
+
+  # get 'posts/edit'
+  resources :posts
+
+  # get 'welcome/index'
+
+  # get 'welcome/about'
+
+  # get 'welcome/contact'
+  
+  get 'about' => 'welcome#about'
   
   root to: 'welcome#index'
 
