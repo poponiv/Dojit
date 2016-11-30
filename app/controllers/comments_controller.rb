7906f83dd6198bc022c8378fde239cb69dac98e2
nivd <niv@simplee.com>
2016-11-30 18:08:05 +0200

    Fully add comments, but add comments has a bug

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,26 @@
+class CommentsController < ApplicationController
+
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+    authorize @comment
+  end
+
+	def create
+		@post = Post.find(params[:post_id])
+  	@comment = current_user.comments.build(comment_params)
+    @comment.post = @post
+    authorize @comment
+  	if @comment.save
+  		flash[:notice] = "Comment was saved."
+      
+  	else
+  		flash[:error] = "There was an error saving the comment. Please try again."
+  	end	
+    redirect_to topic_post_path, {topic_id: @post.topic, id: @post}
+	end
+
+	def comment_params
+		params.require(:comment).permit(:body)
+	end
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
 	belongs_to :post
 	belongs_to :user
+
+	default_scope { order('created_at DESC')}
+	
+	validates :body, length: { minimum: 5 }, presence: true
+	validates :user_id, presence: true
 end
