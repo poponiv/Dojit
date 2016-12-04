b7d77f946e705444c9369011f90fb978669c796a
nivd <niv@simplee.com>
2016-12-04 17:08:20 +0200

    Added Public Profiles

--- app/models/post.rb
+++ app/models/post.rb
@@ -24,6 +24,7 @@ class Post < ApplicationRecord
 	end
 
 	default_scope { order('rank DESC')}
+	scope :visible_to, -> (user) { user ? all : joins(:topic).where('topics.public' => true) }
 	
 	validates :title, length: { minimum: 5 }, presence: true
 	validates :body, length: { minimum: 20 }, presence: true
